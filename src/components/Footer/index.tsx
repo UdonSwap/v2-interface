4d884b8fe52543d62730237b89c8756ee86c71bf
Lajja123 <lajja.lamprostech@gmail.com>
2024-03-11 17:57:01 +0530

    add hover color on the footer links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import X from '../../assets/images/x.png'
 import web from '../../assets/images/web.png'
 import discord from '../../assets/images/discord.png'
 import logo from '../../assets/images/UdonSwapLogo.png'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
@@ -13,32 +13,56 @@ function Footer() {
         <div className="f-sec1">
           <div>
             <h2>Resources</h2>
-            <div className="sec-link">Audit</div>
-            <div className="sec-link">Contracts</div>
-            <div className="sec-link">Documentation</div>
-            <div className="sec-link">Leaderboard</div>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Audit</div>
+            </Link>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Contracts</div>
+            </Link>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Documentation</div>
+            </Link>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Leaderboard</div>
+            </Link>
           </div>
           <div>
             <h2>Tools</h2>
-            <div className="sec-link">Mode Bridge</div>
-            <div className="sec-link">Symbiosis</div>
-            <div className="sec-link">Defil lama</div>
-            <div className="sec-link">Leaderboard</div>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Mode Bridge</div>
+            </Link>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Symbiosis</div>
+            </Link>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Defil lama</div>
+            </Link>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Leaderboard</div>
+            </Link>
           </div>
           <div>
             <h2>Social</h2>
-            <div className="sec-link">Discord</div>
-            <div className="sec-link">X</div>
-            <div className="sec-link">Telegram</div>
-            <div className="sec-link">Blog</div>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Discord</div>
+            </Link>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">X</div>
+            </Link>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Telegram</div>
+            </Link>
+            <Link to={''} className="footerlink">
+              <div className="sec-link">Blog</div>
+            </Link>
           </div>
         </div>
 
         <div className="f-sec2">
           <div className="socialMain">
-            <img src={X} alt="x" />
-            <img src={web} alt="web" />
-            <img src={discord} alt="discord" />
+            <img src={X} alt="x" className="fImg1" />
+            <img src={web} alt="web" className="fImg1" />
+            <img src={discord} alt="discord" className="fImg1" />
           </div>
           <div className="socialMain2">
             <a href="https://beatswap-eta.vercel.app/">
