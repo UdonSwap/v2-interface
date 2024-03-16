4ecd3e26c426d37c3508fae76c11b65e4de77d35
Lajja123 <lajja.lamprostech@gmail.com>
2024-03-16 16:50:30 +0530

    add loading and decimal points in pools page

--- src/pages/AllPool/index.tsx
+++ src/pages/AllPool/index.tsx
@@ -1,101 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import allpool from './allpool.module.css'
-// import data from './data.json'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 export default function Home() {
   const [poolData, setPoolData] = useState([])
-
-  // const getPoolDetails = async () => {
-  //   // ETH price in dollar
-  //   const api_url = process.env.REACT_APP_ETHAPIURL
-  //   let ethPriceDollar: number
-  //   if (api_url) {
-  //     const response = await fetch(api_url)
-  //     const ethPrice = await response.json()
-  //     ethPriceDollar = ethPrice.USD
-  //   }
-
-  //   const graphURL = 'https://api.goldsky.com/api/public/project_clth71vucl2l701uu07ha0im7/subgraphs/udonswap/0.0.1/gn'
-
-  //   const poolQuery = `
-  //   query MyQuery {
-  //     pairs(first: 100) {
-  //       id
-  //       token1 {
-  //         name
-  //         symbol
-  //         derivedETH
-  //       }
-  //       token0 {
-  //         name
-  //         symbol
-  //         derivedETH
-  //       }
-  //       reserve0
-  //       reserve1
-  //     }
-  //   }`
-  //   axios({
-  //     url: graphURL,
-  //     method: 'post',
-  //     data: {
-  //       query: poolQuery
-  //     }
-  //   }).then(async result => {
-  //     const data = result.data.data.pairs
-
-  //     // calculate TVL price in dollar
-  //     for (let i = 0; i < data.length; i++) {
-  //       const token0InEth = data[i].reserve0 * data[i].token0.derivedETH
-  //       const token1InEth = data[i].reserve1 * data[i].token1.derivedETH
-  //       const TVL = (token0InEth + token1InEth) * ethPriceDollar
-  //       data[i]['TVL'] = TVL
-  //     }
-
-  //     // get the daily volume
-  //     const sevenDaysEpoch = 604800
-  //     const currentTimestamp = Math.floor(Date.now() / 1000)
-  //     const time = currentTimestamp - sevenDaysEpoch
-
-  //     for (let j = 0; j < data.length; j++) {
-  //       let volumeData
-  //       let volumeToken0 = 0
-  //       let volumeToken1 = 0
-  //       const dailyVolumeQuery = `
-  //         query MyQuery{
-  //           pairDayDatas(where: {date_gte: ${time}, pairAddress: "${data[j].id}"}){
-  //             dailyVolumeToken0
-  //             dailyVolumeToken1
-  //             reserve0
-  //             reserve1
-  //           }
-  //         }`
-  //       axios({
-  //         url: graphURL,
-  //         method: 'post',
-  //         data: {
-  //           query: dailyVolumeQuery
-  //         }
-  //       }).then(result => {
-  //         volumeData = result.data.data.pairDayDatas
-  //         console.log(volumeData)
-  //         for (let k = 0; k < volumeData.length; k++) {
-  //           volumeToken0 = volumeToken0 + Number(volumeData[k].dailyVolumeToken0)
-  //           volumeToken1 = volumeToken1 + Number(volumeData[k].dailyVolumeToken1)
-  //         }
-  //         const volumeToken0Eth = volumeToken0 * Number(data[j].token0.derivedETH)
-  //         const volumeToken1Eth = volumeToken1 * Number(data[j].token1.derivedETH)
-  //         const volumeTokenDollar = (volumeToken0Eth + volumeToken1Eth) * ethPriceDollar
-  //         data[j]['volume7d'] = volumeTokenDollar
-  //       })
-  //     }
-  //     setPoolData(data)
-  //     console.log(data)
-  //   })
-  // }
-
   const getPoolDetails = async () => {
     // ETH price in dollar
     const api_url = process.env.REACT_APP_ETHAPIURL
@@ -105,9 +14,7 @@ export default function Home() {
       const ethPrice = await response.json()
       ethPriceDollar = ethPrice.USD
     }
-
     const graphURL = 'https://api.goldsky.com/api/public/project_clth71vucl2l701uu07ha0im7/subgraphs/udonswap/0.0.1/gn'
-
     const poolQuery = `
     query MyQuery {
       pairs(first: 100) {
@@ -218,8 +125,8 @@ export default function Home() {
               <tr className={allpool.row}>
                 <th className={allpool.column1}>Pools</th>
                 <th className={allpool.column2}>Liquidity</th>
-                <th className={allpool.column2}>Volume (7d)</th>
-                <th className={allpool.column3}>Fees (7d)</th>
+                <th className={allpool.column3}>Volume (7d)</th>
+                <th className={allpool.column4}>Fees (7d)</th>
               </tr>
             </thead>
           </table>
@@ -227,18 +134,22 @@ export default function Home() {
         <div className={allpool.content}>
           <table className={allpool.table}>
             <tbody>
-              {poolData?.length > 0
-                ? poolData.map((token, index) => (
-                    <tr className={allpool.row} key={index}>
-                      <td className={allpool.column1}>
-                        {token['token0']['symbol']}-{token['token1']['symbol']}
-                      </td>
-                      <td className={allpool.column2}>$ {Math.ceil(token['TVL']).toLocaleString()}</td>
-                      <td className={allpool.column3}>$ {Math.ceil(token['volume7d']).toLocaleString()}</td>
-                      <td className={allpool.column3}>${token['volume7d'] * 0.03}</td>
-                    </tr>
-                  ))
-                : 'no data'}
+              {poolData?.length > 0 ? (
+                poolData.map((token, index) => (
+                  <tr className={allpool.row} key={index}>
+                    <td className={allpool.column1}>
+                      {token['token0']['symbol']}-{token['token1']['symbol']}
+                    </td>
+                    <td className={allpool.column2}>$ {parseFloat(token['TVL']).toFixed(3)}</td>
+                    <td className={allpool.column3}>$ {parseFloat(token['volume7d']).toFixed(3)}</td>
+                    <td className={allpool.column4}>${token['volume7d'] * 0.03}</td>
+                  </tr>
+                ))
+              ) : (
+                <div style={{ textAlign: 'center' }}>
+                  <span className={allpool.loader}></span>
+                </div>
+              )}
             </tbody>
           </table>
         </div>
