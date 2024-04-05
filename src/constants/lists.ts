81689c990ae482e0e36a0df6ffab921b54b24f14
darshitbhalodilampros <162084730+darshitbhalodilampros@users.noreply.github.com>
2024-04-05 15:27:16 +0530

    Update lists.ts: set our api as a default list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,6 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
-// export const DEFAULT_TOKEN_LIST_URL = 'https://api.udonswap.org/tokens'
+// export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+export const DEFAULT_TOKEN_LIST_URL = 'https://api.udonswap.org/tokens'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
